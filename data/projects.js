fa464386f6e3d560fa18ecfe499f4fe271381692
Jon Jackson <ocskier@gmail.com>
2021-04-15 11:52:19 -0400

    fix: match maker link

--- data/projects.js
+++ data/projects.js
@@ -9,192 +9,192 @@ class Project {
 }
 
 const Proj1 = new Project(
-  "http://pouracle-basic.herokuapp.com/",
-  "images/gonzalo-remy-JCIJnIXv7SE-unsplash.jpg",
-  "Pouracle",
-  "https://github.com/ocskier/Pouracle-Basic",
+  'http://pouracle-basic.herokuapp.com/',
+  'images/gonzalo-remy-JCIJnIXv7SE-unsplash.jpg',
+  'Pouracle',
+  'https://github.com/ocskier/Pouracle-Basic',
   ['mern']
 );
 const Proj2 = new Project(
-  "https://ocskier.github.io/Train-Station/",
-  "images/railway-station-619082_1280.jpg",
-  "Train Station",
-  "https://github.com/ocskier/Train-Station",
+  'https://ocskier.github.io/Train-Station/',
+  'images/railway-station-619082_1280.jpg',
+  'Train Station',
+  'https://github.com/ocskier/Train-Station',
   ['html']
 );
 // const Proj3=new Project("https://ocskier.github.io/BLS-Website/","images/christine-mendoza-527612-unsplash.jpg","Brightleaf","https://github.com/ocskier/BLS-Website");
 const Proj4 = new Project(
-  "https://ocskier.github.io/Trivia-Game/",
-  "images/question-mark-1872665_1280.jpg",
-  "Trivia Game",
-  "https://github.com/ocskier/Trivia-Game",
+  'https://ocskier.github.io/Trivia-Game/',
+  'images/question-mark-1872665_1280.jpg',
+  'Trivia Game',
+  'https://github.com/ocskier/Trivia-Game',
   ['html']
 );
 const Proj5 = new Project(
-  "https://ocskier.github.io/Star-Wars-Battle/",
-  "images/main-thumb-t-1785517-200-boztfctkwxbzmfjwurneimbimaluhaxk.jpeg",
-  "Star Wars Battle",
-  "https://github.com/ocskier/Star-Wars-Battle",
+  'https://ocskier.github.io/Star-Wars-Battle/',
+  'images/main-thumb-t-1785517-200-boztfctkwxbzmfjwurneimbimaluhaxk.jpeg',
+  'Star Wars Battle',
+  'https://github.com/ocskier/Star-Wars-Battle',
   ['html']
 );
 const Proj6 = new Project(
-  "https://ocskier.github.io/Tithe/",
-  "images/tithe-favicon.1.png",
-  "Tithe App",
-  "https://github.com/ocskier/Tithe",
+  'https://ocskier.github.io/Tithe/',
+  'images/tithe-favicon.1.png',
+  'Tithe App',
+  'https://github.com/ocskier/Tithe',
   ['html']
 );
 const Proj7 = new Project(
-  "https://ocskier.github.io/Api-Ajax/",
-  "images/window-615427_1920.jpg",
-  "Api Demo",
-  "https://github.com/ocskier/Api-Ajax",
+  'https://ocskier.github.io/Api-Ajax/',
+  'images/window-615427_1920.jpg',
+  'Api Demo',
+  'https://github.com/ocskier/Api-Ajax',
   ['html']
 );
 const Proj8 = new Project(
   null,
-  "images/bot.jpg",
-  "LIRI Bot",
-  "https://github.com/ocskier/LIRI-Node-App",
+  'images/bot.jpg',
+  'LIRI Bot',
+  'https://github.com/ocskier/LIRI-Node-App',
   ['node']
 );
 const Proj9 = new Project(
-  "https://my-match-maker.herokuapp.com/",
-  "images/love-logo-hi.png",
-  "Match Maker",
-  "https://github.com/ocskier/Match-Maker",
-  ['html','express']
+  'https://my-match-maker-app.herokuapp.com/',
+  'images/love-logo-hi.png',
+  'Match Maker',
+  'https://github.com/ocskier/Match-Maker',
+  ['html', 'express']
 );
 // const Proj10=new Project("https://intense-wildwood-45271.herokuapp.com/","images/fabio-alves-773406-unsplash.jpg","Burger App","https://github.com/ocskier/Burger-App");
 const Proj11 = new Project(
-  "https://fierce-basin-24175.herokuapp.com/",
-  "images/fabio-alves-773406-unsplash.jpg",
-  "Serialize Burger",
-  "https://github.com/ocskier/sequelizedBurger",
-  ['express','sql','ts']
+  'https://fierce-basin-24175.herokuapp.com/',
+  'images/fabio-alves-773406-unsplash.jpg',
+  'Serialize Burger',
+  'https://github.com/ocskier/sequelizedBurger',
+  ['express', 'sql', 'ts']
 );
 const Proj12 = new Project(
-  "https://aristotle-app.herokuapp.com/",
-  "images/jeshoots-com-436787-unsplash.jpg",
-  "Aristotle",
-  "https://github.com/ocskier/Aristotle",
-  ['express','sql']
+  'https://aristotle-app.herokuapp.com/',
+  'images/jeshoots-com-436787-unsplash.jpg',
+  'Aristotle',
+  'https://github.com/ocskier/Aristotle',
+  ['express', 'sql']
 );
 // const Proj13=new Project("http://mongoscraps.herokuapp.com/","images/hannah_gibbs_1067876.jpg","Mongo Scraper","https://github.com/ocskier/Mongo-Scraper");
 const Proj14 = new Project(
-  "https://search-react-app.herokuapp.com/",
-  "images/hannah_gibbs_1067876.jpg",
-  "NPR-Music-React",
-  "https://github.com/ocskier/NPR-Music-Search-React",
+  'https://search-react-app.herokuapp.com/',
+  'images/hannah_gibbs_1067876.jpg',
+  'NPR-Music-React',
+  'https://github.com/ocskier/NPR-Music-Search-React',
   ['react']
 );
 const Proj15 = new Project(
-  "https://wishlist-p3t6-app.herokuapp.com/",
-  "images/Screenshot_Proj.png",
-  "WishList",
-  "https://github.com/ocskier/WishList",
-  ['mern','express','react']
+  'https://wishlist-p3t6-app.herokuapp.com/',
+  'images/Screenshot_Proj.png',
+  'WishList',
+  'https://github.com/ocskier/WishList',
+  ['mern', 'express', 'react']
 );
 const Proj16 = new Project(
-  "https://ocskier.github.io/Word-Guess-Game/",
-  "images/Screenshot5.png",
-  "Word Game",
-  "https://github.com/ocskier/Word-Guess-Game",
+  'https://ocskier.github.io/Word-Guess-Game/',
+  'images/Screenshot5.png',
+  'Word Game',
+  'https://github.com/ocskier/Word-Guess-Game',
   ['html']
 );
 const Proj17 = new Project(
-  "https://repl.it/@ocskier/Bamazon",
-  "images/fancycrave.jpg",
-  "Bamazon",
-  "https://github.com/ocskier/Bamazon",
-  ['node','sql']
+  'https://repl.it/@ocskier/Bamazon',
+  'images/fancycrave.jpg',
+  'Bamazon',
+  'https://github.com/ocskier/Bamazon',
+  ['node', 'sql']
 );
 const Proj18 = new Project(
-  "https://ocskier.github.io/Memory/",
-  "images/BN-XN.jpg",
-  "Memory",
-  "https://github.com/ocskier/Memory",
+  'https://ocskier.github.io/Memory/',
+  'images/BN-XN.jpg',
+  'Memory',
+  'https://github.com/ocskier/Memory',
   ['react']
 );
 
 const Proj19 = new Project(
-  "https://yahtzee-react-app.herokuapp.com/",
-  "https://images.vexels.com/media/users/3/135811/isolated/preview/f3dc1094d770aadce0dff261623fddb6-dices-3d-icon-by-vexels.png",
-  "Yahtzee",
-  "https://github.com/ocskier/Yahtzee-Online",
-  ['mern','ts','react']
+  'https://yahtzee-react-app.herokuapp.com/',
+  'https://images.vexels.com/media/users/3/135811/isolated/preview/f3dc1094d770aadce0dff261623fddb6-dices-3d-icon-by-vexels.png',
+  'Yahtzee',
+  'https://github.com/ocskier/Yahtzee-Online',
+  ['mern', 'ts', 'react']
 );
 
 const Proj20 = new Project(
-  "https://ocskier.github.io/QuizGame/",
-  "images/question-mark-1872665_1280.jpg",
-  "Quiz Game",
-  "https://github.com/ocskier/QuizGame",
+  'https://ocskier.github.io/QuizGame/',
+  'images/question-mark-1872665_1280.jpg',
+  'Quiz Game',
+  'https://github.com/ocskier/QuizGame',
   ['html']
 );
 
 const Proj21 = new Project(
   null,
-  "images/sample3__1528944557_50988.jpg",
-  "Employee Database",
-  "https://github.com/ocskier/IncDbCli",
+  'images/sample3__1528944557_50988.jpg',
+  'Employee Database',
+  'https://github.com/ocskier/IncDbCli',
   ['node', 'sql', 'ts']
 );
 
 const Proj22 = new Project(
-  "https://ocskier.github.io/UsrDirTSX/",
-  "images/sample3__1528944557_50988.jpg",
-  "Employee Search",
-  "https://github.com/ocskier/UsrDirTSX",
-  ['react','ts']
+  'https://ocskier.github.io/UsrDirTSX/',
+  'images/sample3__1528944557_50988.jpg',
+  'Employee Search',
+  'https://github.com/ocskier/UsrDirTSX',
+  ['react', 'ts']
 );
 
 const Proj23 = new Project(
   null,
-  "https://user-images.githubusercontent.com/36890724/73634091-60852580-462e-11ea-9275-1d0eeeca137a.png",
-  "Template Engine",
-  "https://github.com/ocskier/HTMLTemplateEngine",
-  ['node','ts']
+  'https://user-images.githubusercontent.com/36890724/73634091-60852580-462e-11ea-9275-1d0eeeca137a.png',
+  'Template Engine',
+  'https://github.com/ocskier/HTMLTemplateEngine',
+  ['node', 'ts']
 );
 
 const Proj24 = new Project(
-  "https://pwdgenerator.herokuapp.com/",
-  "images/question-mark-1872665_1280.jpg",
-  "Pwd Generator",
-  "https://github.com/ocskier/pwdGenFullStack",
-  ['express','html','ts']
+  'https://pwdgenerator.herokuapp.com/',
+  'images/question-mark-1872665_1280.jpg',
+  'Pwd Generator',
+  'https://github.com/ocskier/pwdGenFullStack',
+  ['express', 'html', 'ts']
 );
 
 const Proj25 = new Project(
   null,
-  "images/sample3__1528944557_50988.jpg",
-  "Md Generator",
-  "https://github.com/ocskier/RMGenTS",
-  ['node','ts']
+  'images/sample3__1528944557_50988.jpg',
+  'Md Generator',
+  'https://github.com/ocskier/RMGenTS',
+  ['node', 'ts']
 );
 
 const Proj26 = new Project(
-  "https://tutor-dashboard.herokuapp.com/",
-  "images/sample3__1528944557_50988.jpg",
-  "Tutor-Dashboard",
-  "https://github.com/ocskier/TutorDashboard",
-  ['python','react']
+  'https://tutor-dashboard.herokuapp.com/',
+  'images/sample3__1528944557_50988.jpg',
+  'Tutor-Dashboard',
+  'https://github.com/ocskier/TutorDashboard',
+  ['python', 'react']
 );
 
 const Proj27 = new Project(
-  "https://stock-tracker-101.herokuapp.com/",
-  "images/fabian-blank-pElSkGRA2NU-unsplash.jpg",
-  "Stock Dashboard",
-  "https://github.com/ocskier/MyPythonProj",
-  ["python", "react"]
+  'https://stock-tracker-101.herokuapp.com/',
+  'images/fabian-blank-pElSkGRA2NU-unsplash.jpg',
+  'Stock Dashboard',
+  'https://github.com/ocskier/MyPythonProj',
+  ['python', 'react']
 );
 
 const Proj28 = new Project(
-  "https://ocskier.github.io/Time-Manager/",
-  "images/DayCapture.PNG",
-  "Work Day Scheduler",
-  "https://github.com/ocskier/Time-Manager",
-  ["html"]
+  'https://ocskier.github.io/Time-Manager/',
+  'images/DayCapture.PNG',
+  'Work Day Scheduler',
+  'https://github.com/ocskier/Time-Manager',
+  ['html']
 );
 
 const projectsArray = [];
@@ -226,7 +226,7 @@ projectsArray.push(
   Proj28
 );
 
-const shuffleArray = projects => {
+const shuffleArray = (projects) => {
   const newArray = projects;
   for (let i = newArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -235,22 +235,28 @@ const shuffleArray = projects => {
   return newArray;
 };
 
-$.each(shuffleArray(projectsArray), function(index, project) {
-  $("#portfolio-content").append(`
-    <li class="full-thumb projectLi ${project.tech.map(tech=>`f-${tech}`).join(" ")}">
+$.each(shuffleArray(projectsArray), function (index, project) {
+  $('#portfolio-content').append(`
+    <li class="full-thumb projectLi ${project.tech
+      .map((tech) => `f-${tech}`)
+      .join(' ')}">
         <img src=${project.pic} width=220 height=165 alt=${project.name}/>
         <div class="div-info" style="display: flex;">
         <div style="display: inline-flex;">
-          <a href=${project.url ? project.url : project.ghLink} class="fancybox" rel="group" title=${project.name} target="_blank">
+          <a href=${
+            project.url ? project.url : project.ghLink
+          } class="fancybox" rel="group" title=${project.name} target="_blank">
             <div class="thumb-icon-photo"></div>
           </a>
           <div class="thumb-icon-github">
-            <a id="logo-container" href=${project.ghLink} class="brand-logo" target="_blank">
+            <a id="logo-container" href=${
+              project.ghLink
+            } class="brand-logo" target="_blank">
               <img src="images/GitHub-Mark-Light-120px-plus.png" style="margin: 3px 0 0 0;" width="25px" height="25px">
             </a>
           </div>
         </div>
         <div class="thumb-name">${project.name}</div>
         </div>
-    </li>`)
+    </li>`);
 });
